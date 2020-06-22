9d5707c203d286e1d8105064d3ef916a4b6359be
Jose Matias <josematias92@gmail.com>
2020-06-22 17:57:32 -0500

    Created the method for the controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,28 @@
 class RepliesController < ApplicationController
+  def new
+    @reply = Reply.new
+    @dilemma = Dilemma.find(params[:dilemma_id])
+  end
+
+  def create
+    @reply = Reply.new(reply_params)
+    @reply.dilemma_id = params[:dilemma_id]
+    if @reply.save
+      redirect_to dilemma_path(@dilemma)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @reply.destroy
+
+    redirect_to dilemma_path(@dilemma)
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:answer, :reply_votes)
+  end
 end
