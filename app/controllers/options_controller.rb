be26a68c51bf2a2fda8952cb00ad345f38b251c6
Mariana Galan <gatm870125@gmail.com>
2020-06-23 13:28:55 -0500

    Form for dilemma, without options, works

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,18 +1,19 @@
-
-  class OptionsController < ApplicationController
+class OptionsController < ApplicationController
   def new
     @dilemma = Dilemma.find(params[:dilemma_id])
     @option = Option.new
   end
+
   def create
     @dilemma = Dilemma.find(params[:dilemma_id])
     @option = Option.new(option_params)
     @option.dilemma = @dilemma
     @option.save
   end
+
   private
+
   def option_params
-    params.require(:option).permit(:description, :upvotes, :dilemma_id)
+    params.require(:option).permit(:description)
   end
 end
-
