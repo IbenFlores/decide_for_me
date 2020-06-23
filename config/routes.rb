cbd483718530697fbdab19989ef6f2ae69465318
iben flores <iben.fpolanco@gmail.com>
2020-06-22 19:45:23 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: [:index, :show]
   resources :dilemmas do
-    resources :option, only: [:new, :create]
-    resources :reply, only: [:new, :create]
+    resources :options, only: [:new, :create]
+    resources :replies, only: [:new, :create]
   end
-  resources :reply, only: :destroy
+  resources :replies, only: :destroy
 end
