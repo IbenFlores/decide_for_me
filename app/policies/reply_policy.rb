a432ce355083e04a7e68c17d0a19ab99d5130699
iben flores <iben.fpolanco@gmail.com>
2020-06-23 12:31:25 -0500

    added the replies policy for authorization

---
+++ app/policies/reply_policy.rb
@@ -0,0 +1,15 @@
+class ReplyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
