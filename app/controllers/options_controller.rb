3d2c57b7c1b081a8afaed6935c1a69546ef38611
Jolie Adam <as.jolie3@gmail.com>
2020-06-23 18:02:08 -0400

    options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,20 @@
 class OptionsController < ApplicationController
+  def new
+    @dilemma = Dilemma.find(params[:dilemma_id])
+    @option = Option.new
+  end
+
+  def create
+    @dilemma = Dilemma.find(params[:dilemma_id])
+    @option = Option.new(option_params)
+    @option.dilemma = @dilemma
+
+    @option.save
+  end
+
+  private
+
+  def option_params
+    params.require(:option).permit(:description, :upvotes, :dilemma_id)
+  end
 end
