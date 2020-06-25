7b2503d308861936f52eec55bb9b96cd87e48872
iben flores <iben.fpolanco@gmail.com>
2020-06-25 16:53:37 -0500

    changed the minimun of characters for replies

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,6 @@ class Reply < ApplicationRecord
   belongs_to :dilemma
   belongs_to :owner, class_name: :User, foreign_key: :owner_id
   validates :answer, length: {
-    minimum: 10, maximum: 280
+    minimum: 1, maximum: 280
   }
 end
