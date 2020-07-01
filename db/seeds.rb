dd45cb887aec04f7bf6bd753ed624e7a09f69163
iben flores <iben.fpolanco@gmail.com>
2020-07-01 10:22:30 -0500

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@ User.destroy_all if Rails.env.development?
 puts "DB restored"
 
 puts "creating new users"
-avatar_user1 = URI.open("https://gluc.mx/u/fotografias/m/2019/12/10/f638x638-20876_79043_5050.jpg")
-user1 = User.create!(username: "Michael", email: "michael@decide.com", password: "123456")
-user1.avatar.attach(io: avatar_user1, filename: 'michael.jpg', content_type: 'image/jpg')
+avatar_user1 = URI.open("https://files.slack.com/files-pri/T02NE0241-F016D9JTWDR/image.png")
+user1 = User.create!(username: "Jolie", email: "as.jolie3@gmail.com", password: "123456")
+user1.avatar.attach(io: avatar_user1, filename: 'jolie_avatar.jpg', content_type: 'image/png')
 
-avatar_user2 = URI.open("https://plantillasdememes.com/img/plantillas/shaquille-oneal-sorprendido-viajes-en-el-tiempo01563255669.jpg")
-user2 = User.create!(username: "Jimmy", email: "jimmy@decide.com", password: "123456")
-user2.avatar.attach(io: avatar_user2, filename: 'jimmy.jpg', content_type: 'image/jpg')
+avatar_user2 = URI.open("https://files.slack.com/files-pri/T02NE0241-F015YAWEWDV/picture.jpg")
+user2 = User.create!(username: "Mariana", email: "gatm870125@gmail.com", password: "123456")
+user2.avatar.attach(io: avatar_user2, filename: 'mariana_avatar.jpg', content_type: 'image/jpg')
 
 avatar_user3 = URI.open("https://pbs.twimg.com/media/ES3LuV6UcAMPlHp.jpg")
-user3 = User.create!(username: "Sofia", email: "sofia@decide.com", password: "123456")
-user3.avatar.attach(io: avatar_user3, filename: 'sofia.jpg', content_type: 'image/jpg')
+user3 = User.create!(username: "Jose", email: "josematias92@gmail.com", password: "123456")
+user3.avatar.attach(io: avatar_user3, filename: 'jose_avatar.jpg', content_type: 'image/jpg')
 
 # => First user
 
-puts "creating dilemmas for Michael"
+puts "creating dilemmas for Jolie"
 dilemma_1 = Dilemma.create!(question: "Prom dance outfit selection",
                             category: 2,
                             tag: "clothing",
@@ -51,11 +51,11 @@ dilemma_3_option_2 = Option.create!(description: "Mountain hike",
                                     dilemma_id: dilemma_3.id)
 puts "Dilemma created"
 
-puts "Michael's dilemmas have been created"
+puts "Jolie's dilemmas have been created"
 
 #  => Second user
 
-puts "creating dilemmas for Jimmy"
+puts "creating dilemmas for Mariana"
 dilemma_4 = Dilemma.create!(question: "Wanna watch a movie marathon",
                             category: 0,
                             tag: "movies",
@@ -86,11 +86,11 @@ dilemma_6_option_2 = Option.create!(description: "Xbox S",
                                     dilemma_id: dilemma_6.id)
 puts "Dilemma created"
 
-puts "Jimmy's dilemmas have been created"
+puts "Mariana's dilemmas have been created"
 
 # => Third user
 
-puts "creating dilemmas for Sofia"
+puts "creating dilemmas for Jose"
 dilemma_7 = Dilemma.create!(question: "What should i cook today?",
                             category: 0,
                             tag: "cooking",
@@ -121,6 +121,6 @@ dilemma_9_option_2 = Option.create!(description: "13 Reasons why",
                                     dilemma_id: dilemma_9.id)
 puts "Dilemma created"
 
-puts "Sofia's dilemmas have been created"
+puts "Jose's dilemmas have been created"
 
 
