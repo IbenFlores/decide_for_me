5fe4b3d12b9de693bf9a84e784324536f5abe27d
Jose Matias <josematias92@gmail.com>
2020-07-03 18:08:14 -0500

    Error Message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :answered_dilemmas, :avatar, :bio])
   end
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
